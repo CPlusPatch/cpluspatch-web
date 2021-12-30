dbc904b0b889eab77da971d1b5c2dd5e8f421566
Gascraft971 <gascraft971@gmail.com>
2021-12-30 15:55:26 +0100

    Added initial hero page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,83 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <title>Laravel</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <link href="{{ asset("css/app.css") }}" rel="stylesheet">
+</head>
 
-            .full-height {
-                height: 100vh;
-            }
+<body>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+    <main class="dark:bg-gray-800 bg-white relative overflow-hidden h-screen">
+        <header class="h-24 sm:h-32 flex items-center z-30 w-full">
+            <div class="container mx-auto px-6 flex items-center justify-between">
+                <div class="uppercase text-gray-800 dark:text-white font-black text-3xl">
+                    CPLUSPATCH
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Test
+                <div class="flex items-center">
+                    <nav class="font-sen text-gray-800 dark:text-white uppercase text-lg lg:flex items-center hidden">
+                        <a href="#" class="py-2 px-6 flex">
+                            Home
+                        </a>
+                        <a href="#" class="py-2 px-6 flex">
+                            Socials
+                        </a>
+                        <a href="#" class="py-2 px-6 flex">
+                            Projects
+                        </a>
+                        <a href="#" class="py-2 px-6 flex">
+                            Contact
+                        </a>
+                    </nav>
+                    <button class="lg:hidden flex flex-col ml-4">
+                        <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1">
+                        </span>
+                        <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1">
+                        </span>
+                        <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1">
+                        </span>
+                    </button>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            </div>
+        </header>
+        <div class="bg-white dark:bg-gray-800 flex relative z-20 items-center overflow-hidden">
+            <div class="container mx-auto px-6 flex relative py-16">
+                <div class="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
+                    <span class="w-20 h-2 bg-gray-800 dark:bg-white mb-12">
+                    </span>
+                    <h1
+                        class="font-bebas-neue uppercase text-6xl sm:text-8xl font-black flex flex-col leading-none dark:text-white text-gray-800">
+                        Gaspard
+                        <span class="text-5xl sm:text-7xl">
+                            Wierzbinski
+                        </span>
+                    </h1>
+                    <p class="text-sm sm:text-base text-gray-700 dark:text-white mt-8">
+                        My projects and my stuff is here
+                    </p>
+                    <div class="flex mt-8">
+                        <a href="#"
+                            class="uppercase py-2 px-4 rounded-lg bg-pink-500 border-2 border-transparent text-white text-md mr-4 hover:bg-pink-400">
+                            Check out my stuff
+                        </a>
+                        <a href="#"
+                            class="uppercase py-2 px-4 rounded-lg bg-transparent border-2 border-pink-500 text-pink-500 dark:text-white hover:bg-pink-500 hover:text-white text-md">
+                            My socials
+                        </a>
+                    </div>
+                </div>
+                <div class="hidden sm:block sm:w-1/3 lg:w-3/5 relative">
+                    <img src="/cdn/hero.png" class="w-xl md:w-lg m-auto translate-x-56" />
                 </div>
             </div>
         </div>
-    </body>
+    </main>
+</body>
+
 </html>
