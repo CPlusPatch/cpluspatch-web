c1b582e5987396cc3a872313d6432635d9177384
Gascraft971 <gascraft971@gmail.com>
2021-12-30 18:30:15 +0100

    Added favicon

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,10 +1,10 @@
 <header class="h-24 sm:h-32 flex items-center z-30 w-full">
 	<div class="container mx-auto px-6 flex items-center justify-between">
-		<div class="uppercase text-gray-800 dark:text-white font-black text-3xl">
+		<div class="uppercase text-white font-black text-3xl">
 			CPLUSPATCH
 		</div>
 		<div class="flex items-center">
-			<nav class="font-sen text-gray-800 dark:text-white uppercase text-lg lg:flex items-center hidden">
+			<nav class="font-thin text-white uppercase text-lg lg:flex items-center hidden">
 				<a href="/" class="py-2 px-6 flex">
 					Home
 				</a>
