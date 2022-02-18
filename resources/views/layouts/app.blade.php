84d9b0f6309f23bc9e033e370613799230ad0330
Gaspard Wierzbinski <gaspard.wierzbinski@gmail.com>
2022-02-18 15:29:43 +0100

    Home page updated and projects page updated as well

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full w-full">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="w-full h-full">
 
 <head>
     <meta charset="utf-8">
@@ -17,12 +17,10 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 	<!-- jQuery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <!-- Bootstrap JS -->
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> --}}
 </head>
 
-<body class="h-full w-full">
-	<main class="bg-gray-800 relative overflow-scroll h-screen">
+<body class="w-full h-full">
+	<main class="relative h-screen overflow-scroll bg-gray-800">
 		@yield("content")
 	</main>
 	<script src="{{ asset("js/js.cookie.min.js") }}"></script>
--- resources/views/socials.blade.php
+++ resources/views/socials.blade.php
@@ -4,7 +4,7 @@
 
 @section("content")
 	@if (Request::get("use-this-page-you-fool") == 1)
-	<div role="alert" style="width: 100vw;" class="container-fluid bg-blue-500 flex items-center text-white text-sm font-bold px-4 py-2 relative">
+	<div role="alert" style="width: 100vw;" class="relative flex items-center px-4 py-2 text-sm font-bold text-white bg-blue-500 container-fluid">
 		<svg width="20" height="20" fill="currentColor" class="w-4 h-4 mr-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
 			<path d="M1216 1344v128q0 26-19 45t-45 19h-512q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h64v-384h-64q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h384q26 0 45 19t19 45v576h64q26 0 45 19t19 45zm-128-1152v192q0 26-19 45t-45 19h-256q-26 0-45-19t-19-45v-192q0-26 19-45t45-19h256q26 0 45 19t19 45z">
 			</path>
@@ -19,19 +19,19 @@
 
 
 <div class="container mx-auto">
-<div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 justify-items-center">
-	@include("components.socials.profile", ["url" => "#", "logo" => "/cdn/discord.webp", "name" => "Discord", "username" => "CPlusPatch<b>#9373</b>", "copy" => "CPlusPatch#9373"])
-	@include("components.socials.profile", ["url" => "#", "logo" => "/cdn/github.svg", "name" => "GitHub", "username" => "CPlusPatch", "copy" => "https://github.com/CPlusPatch"])
+<div class="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center">
+	@include("components.socials.profile", ["url" => "#", "logo" => "/images/discord.webp", "name" => "Discord", "username" => "CPlusPatch<b>#9373</b>", "copy" => "CPlusPatch#9373"])
+	@include("components.socials.profile", ["url" => "#", "logo" => "/images/github.svg", "name" => "GitHub", "username" => "CPlusPatch", "copy" => "https://github.com/CPlusPatch"])
 	{{-- @include("components.socials.profile", ["url" => "#", "logo" => "/cdn/reddit.png", "name" => "Reddit", "username" => "Gascraft971", "copy" => "https://www.reddit.com/user/Gascraft971"]) --}}
-	@include("components.socials.profile", ["url" => "#", "logo" => "/cdn/spotify.jpg", "name" => "Spotify", "username" => "CPlusPatch", "copy" => "https://open.spotify.com/user/ulzj1q1muil9sjy87f4krzdoa"])
-	@include("components.socials.profile", ["url" => "#", "logo" => "/cdn/steam.png", "name" => "Steam", "username" => "CPlusPatch", "copy" => "https://steamcommunity.com/id/cpluspatch/"])
-	@include("components.socials.profile", ["url" => "#", "logo" => "/cdn/twitch.jpg", "name" => "Twitch", "username" => "CPlusPatch", "copy" => "https://www.twitch.tv/cpluspatch"])
+	@include("components.socials.profile", ["url" => "#", "logo" => "/images/spotify.jpg", "name" => "Spotify", "username" => "CPlusPatch", "copy" => "https://open.spotify.com/user/ulzj1q1muil9sjy87f4krzdoa"])
+	@include("components.socials.profile", ["url" => "#", "logo" => "/images/steam.png", "name" => "Steam", "username" => "CPlusPatch", "copy" => "https://steamcommunity.com/id/cpluspatch/"])
+	@include("components.socials.profile", ["url" => "#", "logo" => "/images/twitch.jpg", "name" => "Twitch", "username" => "CPlusPatch", "copy" => "https://www.twitch.tv/cpluspatch"])
 	{{-- @include("components.socials.profile", ["url" => "#", "logo" => "/cdn/twitter.png", "name" => "Twitter", "username" => "CPlusPatch", "copy" => "https://twitter.com/CPlusPatch"]) --}}
-	@include("components.socials.profile", ["url" => "#", "logo" => "/cdn/instagram.png", "name" => "Instagram", "username" => "gaspard_wski", "copy" => "https://www.instagram.com/gaspard_wski/"])
-	@include("components.socials.profile", ["url" => "#", "logo" => "/cdn/mail.png", "name" => "Mail (Personal)", "username" => "gascraft971@gmail.com", "copy" => "gascraft971@gmail.com"])
-	@include("components.socials.profile", ["url" => "#", "logo" => "/cdn/mail.png", "name" => "Mail (Work)", "username" => "contact@cpluspatch.com", "copy" => "contact@cpluspatch.com"])
+	@include("components.socials.profile", ["url" => "#", "logo" => "/images/instagram.png", "name" => "Instagram", "username" => "gaspard_wski", "copy" => "https://www.instagram.com/gaspard_wski/"])
+	@include("components.socials.profile", ["url" => "#", "logo" => "/images/mail.png", "name" => "Mail (Personal)", "username" => "gascraft971@gmail.com", "copy" => "gascraft971@gmail.com"])
+	@include("components.socials.profile", ["url" => "#", "logo" => "/images/mail.png", "name" => "Mail (Work)", "username" => "contact@cpluspatch.com", "copy" => "contact@cpluspatch.com"])
 	{{-- @include("components.socials.profile", ["url" => "#", "logo" => "/cdn/youtube.png", "name" => "YouTube", "username" => "CPlusPatch 2544", "copy" => "https://www.youtube.com/channel/UC-EH3na_o6uyVD6tmTZoaOA"]) --}}
-	@include("components.socials.profile", ["url" => "#", "logo" => "/cdn/dndbeyond.png", "name" => "D&D Beyond", "username" => "CPlusPatch4502", "copy" => "https://www.dndbeyond.com/members/CPlusPatch4502"])
+	@include("components.socials.profile", ["url" => "#", "logo" => "/images/dndbeyond.png", "name" => "D&D Beyond", "username" => "CPlusPatch4502", "copy" => "https://www.dndbeyond.com/members/CPlusPatch4502"])
 </div>
 </div>
 
